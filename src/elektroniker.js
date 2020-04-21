1d922eb91a5779dff3d6461260a5d6ff2b2bcb05
denjutv <info@denju.tv>
2020-04-21 21:35:20 +0200

    parse arguments and config

--- src/elektroniker.js
+++ src/elektroniker.js
@@ -3,8 +3,41 @@ class Elektroniker {
 
   }
 
-  run( options ) {
+  run( ) {
+    this.parseArgv();
+    this.parseConfig();
+  }
+
+  parseArgv( ) {
+    const argv = require('minimist')(process.argv.slice(2));
+
+    const defaultArgv = {
+    };
+
+    this.argv = Object.assign( {}, defaultArgv, argv );
+  }
+
+  parseConfig( ) {
+    const path = require( "path" );
+    const defaultConfig = {
+    };
+
+    let configPath = "";
     
+    if( this.argv.config ) {
+      configPath = path.join( process.cwd(), this.argv.config );
+    }
+    else {
+      const findup = require( "findup-sync" );
+      const findupPath = findup( "elektroniker.config.js" );
+      if( findupPath ) {
+        configPath = path.resolve(findupPath);
+      }
+    }
+
+    const config = configPath ? require( configPath ) : {};
+
+    this.config = Object.assign( {}, defaultConfig, config );
   }
 }
 
