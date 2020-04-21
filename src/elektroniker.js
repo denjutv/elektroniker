f2ec7412344739df0d3ff2301eb66971b516c72a
denjutv <info@denju.tv>
2020-04-21 22:16:49 +0200

    implemented executeHook method and call onBeforeStart hook before running elektron

--- src/elektroniker.js
+++ src/elektroniker.js
@@ -1,11 +1,21 @@
+const execa = require( "execa" );
+
 class Elektroniker {
   constructor() {
 
   }
 
-  run( ) {
+  async run( ) {
     this.parseArgv();
     this.parseConfig();
+
+    // run before start hook
+    this.executeHook( this.config.onBeforeStart );
+
+    // start application
+    const subprocess = execa( "electron", [this.config.entry].concat(this.config.args) );
+
+
   }
 
   parseArgv( ) {
@@ -20,6 +30,13 @@ class Elektroniker {
   parseConfig( ) {
     const path = require( "path" );
     const defaultConfig = {
+      entry: "./src/index.js",  // path to entry file
+      args: [],                 // array of command line arguments for electron
+      watchPath: "./src",       // single string || object { main, render }
+      onBeforeStart: null,      // function (return promise) || string || object { command, args }
+      onEnd: null,
+      onMainChange: null,
+      onRenderChange: null
     };
 
     let configPath = "";
@@ -38,6 +55,29 @@ class Elektroniker {
     const config = configPath ? require( configPath ) : {};
 
     this.config = Object.assign( {}, defaultConfig, config );
+  }
+
+  async executeHook( hook ) {
+    const type = typeof hook;
+    
+    // in case hook is null, which also resolves in type === "object"
+    if( !hook ) {
+      return;
+    }
+
+    switch( type ) {
+      case "function":
+        await hook();
+        break;
+      case "string":
+        const {stdoutStr} = await execa( hook );
+        console.log(stdoutStr);
+        break;
+      case "object":
+        const {stdoutObj} = await execa( hook.command, hook.args );
+        console.log(stdoutObj);
+        break;
+    }
   }
 }
 
